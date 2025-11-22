67d3b836e30045f3a78591946180d209a4f0f8a2
Qasim509 <hq74248@gmail.com>
2025-11-22 03:49:07 -0800

    i

--- app.js
+++ app.js
@@ -11,13 +11,64 @@ var passwordError = document.getElementById('passwordError')
 var ageError = document.getElementById('ageError')
 var contactError = document.getElementById('contactError')
 
-console.log(userName)
-console.log(password)
-console.log(age)
-console.log(contact)
-console.log(email)
-console.log(userNameError)
-console.log(passwordError)
-console.log(emailError)
-console.log(ageError)
-console.log(contactError)
+function submit(){
+
+    //Name validation
+
+    if (userName.value == ""){
+        userNameError.innerText = 'Please enter your name.'
+    }
+    else if(userName.value.length < 5){
+        userNameError.innerText = "Username must be greater than 5 letters."
+    }
+    else{
+        userNameError.innerText = ""
+    }
+
+    //Email validation
+
+    if (email.value == ""){
+        emailError.innerText = "Please enter your email address."
+    }
+    else if(!email.value.includes("@")){
+        emailError.innerText = "Enter correct email address."
+    }
+    else{
+        emailError.innerText = ""
+    }
+
+    //Password validation
+
+    if(password.value == ""){
+        passwordError.innerText = "Please enter a password"
+    }
+    else if(password.value.length < 8){
+        passwordError.innerText = "Password must be greater than or equal to 8 characters"
+    }
+    else{
+        passwordError.innerText = ""
+    }
+
+    //Age error
+    if(age.value == 0){
+        ageError.innerText = "Please enter your age"
+    }
+    else if(age.value < 18){
+        ageError.innerText = "Your age must be greater than 18 to be eligible for cnic"
+    }
+    else{
+        ageError.innerText = ""
+    }
+
+    //contact validation
+
+    if(contact.value == 0){
+        contactError.innerText = "Please enter your contact number"
+    }
+    else if(contact.value != 11){
+        contactError.innerText = "Contact number must be of 11 numbers"
+    }
+    else{
+        contactError.innerText = ""
+    }
+}
